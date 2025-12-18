9406cb43c7a212bc757e765179f82bf461850cee
MykhailoBul <mykhailo.bulava@voco.ee>
2025-12-18 10:57:57 +0200

    Change import statement for Product

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import Product from './constructors/Product.js';
+import { Product } from './constructors/Product.js';
 import { displayAllProductsView } from "./views/allProductsView.js"; 
 import { navigate } from './router.js';
 
@@ -8,7 +8,6 @@ const products = [
     new Product(3, 'Headphones', 199.99, 'Audio', 'images/headphones.webp'),
     new Product(4, 'Smartwatch', 299.99, 'Wearables', 'images/smartwatch.avif'),
 ];
-
 
 const initApp = () => {
     const homeButton = document.getElementById('home-button');
